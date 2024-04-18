b9f99d51d4e44f6abed9e127820c2634694f0fd2
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2024-04-18 12:50:03 +0700

    update train script

--- train.py
+++ train.py
@@ -3,25 +3,27 @@ from ultralytics import YOLO
 '''
     Bagian Ini Untuk Fresh Training
 '''
-model = YOLO('yolov8x-seg.yaml')  # build a new model from YAML
-model = YOLO('yolov8x-seg.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8x-seg.yaml').load('yolov8x.pt')  # build from YAML and transfer weights
+# model = YOLO('yolov8n-seg.yaml')  # build a new model from YAML
+# model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8n-seg.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
-# '''
-#     Bagian Ini Untuk Pre-Trained
-# '''
-# model = YOLO('runs/segment/train53/weights/best.pt')
+'''
+    Bagian Ini Untuk Pre-Trained
+'''
+model = YOLO('runs/segment/train02/weights/best.pt')
 
 
-results = model.train(
-    data='data.yaml', 
-    epochs=10000, 
-    imgsz=320, 
-    name='train5', 
-    # resume=True, 
-    amp=False,
-    batch=4, 
-    patience=0, 
-    mask_ratio=1,
-    cls=1.0,
-)
+if __name__ == '__main__':
+    results = model.train(
+        data='data.yaml', 
+        epochs=5, 
+        imgsz=320, 
+        name='train02', 
+        # resume=True, 
+        amp=False,
+        batch=2, 
+        patience=0, 
+        mask_ratio=2,
+        cls=1.0,
+        workers=2,
+    )
